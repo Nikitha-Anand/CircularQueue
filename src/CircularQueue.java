a1d4b3959e9f44899abf7e83574527d6c3e01170
Nikitha-Anand <nikitha.rubesh@gmail.com>
2024-12-06 00:30:10 +0530

    Update CircularQueue.java

--- src/CircularQueue.java
+++ src/CircularQueue.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 import java.util.Scanner;
 
 public class CircularQueue {
@@ -6,28 +7,32 @@ public class CircularQueue {
     public static Integer TailPointer = 0;
     public static Integer NumberOfItems = 0;
 
-    public static Boolean Enqueue(String DataToAdd){
+    public static void Enqueue(String DataToAdd){
         if(NumberOfItems == 10){
             System.out.println("Queue if full; Unsuccessful attempt to add Data Item");
-            return false;
+            return;
         }
         QueueArray[TailPointer] = DataToAdd;
         if(TailPointer >= 9){
-            TailPointer = -1;
+            TailPointer = 0;
         }else{
             TailPointer = TailPointer + 1;
         }
         NumberOfItems = NumberOfItems + 1;
         System.out.println("Data Item Successfully added to the queue");
-        return true;
     }
 
     public static String Dequeue(){
-        if(TailPointer == 0){
-            System.out.println("Dequeue Unsuccessful");
-            return "FALSE";
+        if(NumberOfItems == 0){
+            System.out.println("Dequeue Unsuccessful! Queue is Empty!");
+            return "null";
+        }
+        if(HeadPointer >= 9) {
+            HeadPointer = 0;
         }
         HeadPointer = HeadPointer + 1;
+        NumberOfItems = NumberOfItems - 1;
+        System.out.println("Dequeue Successful!");
         return QueueArray[HeadPointer - 1];
     }
 
@@ -36,15 +41,17 @@ public class CircularQueue {
         String DataItem;
         String Operation;
         do{
-            System.out.println("Enter Operation to perform (Enqueue/Dequeue) or no to stop: ");
+            System.out.println("Enter Operation to perform (Enqueue/Dequeue) or \"NO\" to stop: ");
             Operation = keyboard.next();
-            if(Operation.toLowerCase().equals("enqueue")){
+            if(Operation.equalsIgnoreCase("enqueue")){
                 System.out.println("Enter an item to the queue: ");
                 DataItem = keyboard.next();
                 Enqueue(DataItem);
-            } else if (Operation.toLowerCase().equals("dequeue")) {
-                System.out.println("Dequeued Value: " + Dequeue());
+            } else if (Operation.equalsIgnoreCase("dequeue")) {
+                System.out.println("\nDequeued Value: " + Dequeue());
+            } else if (!Operation.equalsIgnoreCase("no")){
+                System.out.println("Sorry! It is not a valid operation! Try Enqueue/Dequeue or \"NO\" to exit");
             }
-        }while(!Operation.equals("no"));
+        }while(!Operation.equalsIgnoreCase("no"));
     }
 }
